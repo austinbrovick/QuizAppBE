9fe86ffbef9a8f78b14f92d3234eacb8175c2bcb
Austin Brovick <austinbrovick@gmail.com>
2021-01-16 09:35:35 -0800

    Added batch_create_questions

--- app.py
+++ app.py
@@ -9,25 +9,42 @@ import aws_controller
 class Quiz(Resource):
 
     def get(self):
+        category = request.args.get('category')
         temp = jsonify(aws_controller.get_items())
         return jsonify(aws_controller.get_items())
 
     def post(self):
         data = request.json
-        temp = jsonify(aws_controller.create_quiz_question())
+        question = data.get('question')
+        incorrect_answers = data.get('incorrect_answers')
+        correct_answer = data.get('correct_answer')
+        category = data.get('category')
+
+        # just ensure fields are present
+        if not all([bool(question), bool(incorrect_answers), bool(correct_answer), bool(category)]):
+            raise ValueError()
+
+        temp = jsonify(aws_controller.create_quiz_question(
+                question, incorrect_answers, correct_answer, category
+            )
+        )
         return temp.json
 
 
-class Question(Resource):
-    def get(self):
+class Questions(Resource):
 
-        temp = aws_controller.get_question('4c31b7744fd811ebb4b3acde48001122')
-        print(temp['Items'][0]['questions'])
-        return 
+    def post(self):
+        data = request.json
+        questions = data.get('questions')
+        if not type(questions) is list: 
+            raise ValueError("questions not provided")
+
+        # TODO add more validation
+        return aws_controller.batch_create_questions(questions)
 
 
 api.add_resource(Quiz, '/quiz')
-api.add_resource(Question, '/question')
+api.add_resource(Questions, '/questions')
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
--- aws_controller.py
+++ aws_controller.py
@@ -4,12 +4,26 @@ import uuid
 
 dynamo_client = boto3.client('dynamodb')
 
+
 def get_items():
     return dynamo_client.scan(
         TableName='QuizTable'
     )
 
-def create_quiz_question():
+
+def create_quiz_question(question, incorrect_answers, correct_answer, category):
+    if type(question) is not str:
+        raise ValueError()
+
+    if type(incorrect_answers) is not list:
+        raise ValueError()
+
+    if type(correct_answer) is not str:
+        raise ValueError()
+
+    if len(incorrect_answers) != 3:
+        raise ValueError("Must provide 4 answers")
+
     return dynamo_client.put_item(
         TableName='QuizTable',
         Item={
@@ -17,13 +31,20 @@ def create_quiz_question():
                 'S': uuid.uuid1().hex,
             },
             'category': {
-                'S': 'sports'
+                'S': category,
             },
-            'questions': {
-                'L': [{'S': 'HELLO WORLD Q'}, {'S': 'Q2'}]
-            }
+            'question': {
+                'S': question,
+            },
+            'incorrect_answers': {
+                'L': [{'S': answer} for answer in incorrect_answers]
+            },
+            'correct_answer': {
+                'S': correct_answer,
+            },
         }
     )
+
 
 def get_question(id):
     return dynamo_client.query(
@@ -31,5 +52,40 @@ def get_question(id):
         KeyConditionExpression='id = :id',
         ExpressionAttributeValues={
             ':id': {'S': id}
+        }
+    )
+
+# format with Shift + Option + F
+
+
+def batch_create_questions(questions):
+    put_requests = [
+        {
+            'PutRequest': {
+                'Item': {
+                    'id': {
+                        'S': uuid.uuid1().hex,
+                    },
+                    'category': {
+                        'S': q['category']
+                    },
+                    'question': {
+                        'S': q['question']
+                    },
+                    'correct_answer': {
+                        'S': q['correct_answer']
+                    },
+                    'incorrect_answers': {
+                        'L': [{'S': answer} for answer in q['incorrect_answers']]
+                    }
+                }
+            }
+        } for q in questions
+    ]
+
+
+    dynamo_client.batch_write_item(
+        RequestItems={
+            'QuizTable': put_requests
         }
     )
